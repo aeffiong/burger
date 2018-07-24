f277eb8ba77ca90785cf8306649ba8d54cfe99a2
aeffiong <aeffiong9@gmail.com>
2018-07-24 12:50:18 -0600

    got database up and connection to db and node up

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+
+
+// Dependencies
+var mysql = require("mysql");
+var password = require("./config");
+
+// setting up mysql connection
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3307,
+    user: "root",
+    password: password.password,
+    database: "burgers_db"
+});
+
+// Make connection
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// export for ORM to use
+module.exports = connection;
