7c6b5654d3c20314684d6ae0bfa59ba3a7186ba7
aeffiong <aeffiong9@gmail.com>
2018-08-01 22:05:20 -0600

    updated schema and got devoured button to work!

--- public/assests/js/burgers.js
+++ public/assests/js/burgers.js
@@ -1,25 +1,4 @@
 // creating the funtionality of the devour it button
-// $(function() {
-//     $(".change-sleep").on("click", function(event) {
-//       var id = $(this).data("id");
-//       var newSleep = $(this).data("newsleep");
-  
-//       var newSleepState = {
-//         sleepy: newSleep
-//       };
-  
-//       // Send the PUT request.
-//       $.ajax("/api/cats/" + id, {
-//         type: "PUT",
-//         data: newSleepState
-//       }).then(
-//         function() {
-//           console.log("changed sleep to", newSleep);
-//           // Reload the page to get the updated list
-//           location.reload();
-//         }
-//       );
-//     });
 $(function() {
     $("#submit").on("click", function(event) {
         var id = $(this).data("id");
@@ -29,11 +8,12 @@ $(function() {
         };
 
         // send PUT request
-        $.ajax("/burgers/update/" + id, {
+        $.ajax("/burgers/" + id, {
             type: "PUT",
             data: beenEaten
         }).then(function() {
             location.reload();
+            console.log("clicked")
         });
     });
 
