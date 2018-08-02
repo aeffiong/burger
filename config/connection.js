4e4fc66e43a0f4b14fbc8e40bc80ab98a2737487
aeffiong <aeffiong9@gmail.com>
2018-08-01 21:07:55 -0600

    creating the JAWSDB connection for Heroku

--- config/connection.js
+++ config/connection.js
@@ -4,14 +4,23 @@
 var mysql = require("mysql");
 var password = require("./config");
 
-// setting up mysql connection
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3307,
-    user: "root",
-    password: password.password,
-    database: "burgers_db"
-});
+// setting up mysql connection for heroku
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3307,
+        user: "root",
+        password: password.password,
+        database: "burgers_db"
+    });
+};
+
+
+
 
 // Make connection
 connection.connect(function(err) {
